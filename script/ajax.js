422bade73ce4e3a100265ef2d9e7691838996e14
Charles Yeh <charles_yeh@brown.edu>
2011-12-27 01:05:30 -0500

    added ajax

---
+++ script/ajax.js
@@ -0,0 +1,30 @@
+
+function result() {
+	if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+		if (xmlhttp.responseText == 'SUCCESS') {
+			
+		}
+		else if (xmlhttp.responseText == 'ALREADYVOTED') {
+			
+		}
+    }
+}
+
+function request(reqlink, data, result){
+	if (window.XMLHttpRequest)
+	{
+		// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{
+		// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+	xmlhttp.onreadystatechange = result;
+	xmlhttp.open("POST", reqlink, true);
+	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xmlhttp.send(data);
+}
+
